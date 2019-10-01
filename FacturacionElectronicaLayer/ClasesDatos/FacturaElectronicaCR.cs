c8b6563c9911242a67e05b3313753f6d4d972e54
Walter Alpízar <walpizar@hotmail.com>
2019-10-01 17:40:22 -0600

    Compras error en codigo actividad

--- FacturacionElectronicaLayer/ClasesDatos/FacturaElectronicaCR.cs
+++ FacturacionElectronicaLayer/ClasesDatos/FacturaElectronicaCR.cs
@@ -858,7 +858,7 @@ namespace FacturacionElectronicaLayer.ClasesDatos
 
                 // La clave se crea con la función CreaClave de la clase Datos
                 writer.WriteElementString("Clave", _numeroClave);
-                writer.WriteElementString("CodigoActividad", _emisor.codigoActividad.ToString().PadLeft(6, '0'));
+                writer.WriteElementString("CodigoActividad", _empresa.codigoActComercial.ToString().PadLeft(6, '0'));
 
 
 
